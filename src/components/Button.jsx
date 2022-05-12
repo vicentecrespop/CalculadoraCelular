c0068fb7ecdaf95b00c49c145bdfdef2eec7a286
vicentecrespop <vicentecrespo45@gmail.com>
2022-05-12 15:33:59 -0300

    Botões

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Button.css'
+
+export default props => {
+    return <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={`
+            button
+            ${props.double ? 'double' : ''}
+            ${props.operation ? 'operation' : ''}
+            ${props.triple ? 'triple' : ''}
+            ${props.equal ? 'equal' : ''}
+            `}>
+                {props.label}
+            </button>
+}
