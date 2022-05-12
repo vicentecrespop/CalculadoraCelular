fcead5a29ef658a0370fdbb67b395c0d4c63eb66
vicentecrespop <vicentecrespo45@gmail.com>
2022-05-12 15:35:17 -0300

    Notch

---
+++ src/components/Notch.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './Notch.css'
+
+import { FcChargeBattery } from 'react-icons/fc'
+import { BsWifi } from 'react-icons/bs'
+import { BiVolumeMute } from 'react-icons/bi'
+
+const dataAtual = new Date()
+let horas = `${dataAtual.getHours() + 1}`
+let minutos = `${dataAtual.getMinutes()}`
+minutos = minutos.length === 1 ? `0${minutos}` : minutos
+horas = horas.length === 1 ? `0${horas}` : horas
+
+export default props => {
+    return (
+        <div className="notch">
+            <div className="horario">
+                <span>{horas}:{minutos}</span>
+            </div>
+            <div className="dados">
+                <BiVolumeMute className="audio"/>
+                <BsWifi className="wifi"/>
+                <span>100%</span>
+                <FcChargeBattery className="bateria"/>
+            </div>
+        </div>
+    )
+}
