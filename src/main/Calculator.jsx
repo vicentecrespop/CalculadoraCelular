441d81e78e265e64cd9793fc5f19789c8fd4ca8c
vicentecrespop <vicentecrespo45@gmail.com>
2022-05-12 19:36:12 -0300

    -

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -61,6 +61,7 @@ export default class Calculator extends Component {
             case '/': return n1 / n2
             case '+': return n1 + n2
             case '-': return n1 - n2
+            default: console.log('Erro!! Operação nao existe.')
         }
     }
 
